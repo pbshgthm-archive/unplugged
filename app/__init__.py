e647e78e4b29fc197a8f68bb811cb8985fb7f887
Tinkidinki <mahathi98@gmail.com>
2017-04-16 21:35:34 +0530

    final_whatever_i_could_do

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ from app.controls.user_cont import mod_user
 from app.controls.frame_cont import mod_frame
 
 #from app.controls.canvas_cont import mod_article, mod_comment
-from app.controls.canvas_cont_version2 import mod_article, mod_comment
+from app.controls.canvas_cont import mod_article, mod_comment
 
 
 app.register_blueprint(mod_user)
