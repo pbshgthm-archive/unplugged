3d22ce9403bd44f831d650d7b403a2ebccb03fe9
Tinkidinki <mahathi98@gmail.com>
2017-04-28 22:06:22 +0530

    documented code - partly

--- app/controls/circle_cont.py
+++ app/controls/circle_cont.py
@@ -1,3 +1,6 @@
+#The following file defines mod_user, a blueprint instance.
+#All user related routes will be defined as mod_user from now on.
+
 from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for, jsonify
 from sqlalchemy.orm import load_only              
@@ -8,6 +11,8 @@ from app import db
 mod_user = Blueprint('circle', __name__)
 
 
+#The following route takes a user id and returns a HTML page corresponding 
+# to all the circles followed by that user.
 @mod_user.route('/circle/<handle>')
 def viewprofile(handle):
 	user= Circle.query.filter_by(handle=handle).first()
